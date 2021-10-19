16e8f97a32494512168511c61890680291787a97
Bozidarboze <Bozidar_boze123@hotmail.com>
2021-10-19 02:41:32 +0200

    Sign up with firebase

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+import "./sign-in.styles.scss";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput name='email' type='email' value={this.state.email} required onChange={this.handleChange} label='Email' />
+          <FormInput name='password' type='password' value={this.state.password} required onChange={this.handleChange} label='Password' />
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In With Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;

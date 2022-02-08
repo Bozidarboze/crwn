dcba43e0c05a2874797448eee72835ee381f8872
Bozidarboze <Bozidar_boze123@hotmail.com>
2022-02-09 00:59:05 +0100

    Finishing touches

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, history, match }) => {
   return (
     <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
+      <h1
+        className='title'
+        onClick={() => {
+          history.push(`${match.url}/${title.toLowerCase()}`);
+        }}>
+        {title.toUpperCase()}
+      </h1>
       <div className='preview'>
         {items
           .filter((item, idx) => idx < 4)
@@ -18,4 +25,4 @@ const CollectionPreview = ({ title, items }) => {
   );
 };
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
